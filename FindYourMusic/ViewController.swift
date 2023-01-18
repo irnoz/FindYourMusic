d27af3917b200fa5d53858eb62638412a58dafa3
irakli nozadze <iraklinozadze2509@gmail.com>
2023-01-18 18:41:13 +0400

    initial commit

--- FindYourMusic/ViewController.swift
+++ FindYourMusic/ViewController.swift
@@ -7,13 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// User Interface (tableView for list of results and a field to search for songs)
+// network request to get information about songs from spotify API
+// gesture recogniser to see detailed info about a song
+// custom cell to show music info
 
+class ViewController: UIViewController,UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var textField: UITextField!
+    
+    var songs = [Song]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textField.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+    // textField
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchMusic()
+        return true
+    }
+    
+    func searchMusic() {
+        textField.resignFirstResponder()
+    }
+    
+    // tableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        songs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // show song details
+    }
 
 }
 
+struct Song {
+    
+}
